5a9a4b0ebc8f7a5cf903440cba18a834e58400c7
Jacob <dokygamr@outlook.com>
2022-09-28 21:41:38 -0700

    same

--- js/index.js
+++ js/index.js
@@ -1,46 +1,105 @@
-
-function toggleText(){
+function toggleText() {
     var text = document.getElementById("toggleButton");
     if (text.innerHTML === "Off") {
         text.innerHTML = "On";
     } else {
         text.innerHTML = "Off";
     }
-};
+}
 
-function getName(){
+function getName() {
+    let inputName = document.getElementById("nameBox").value;
+    inputName = inputName.trim();
+    let outputText = document.getElementById("nameOutputText");
+    let nameLength = inputName.length;
+
+    if (inputName == "") {
+        outputText.innerHTML = "Please enter a name!";
+    } else {
+        document.getElementById("uppercaseBtn").style.display = "inline";
+        document.getElementById("lowercaseBtn").style.display = "inline";
+        document.getElementById("normalBtn").style.display = "inline";
+
+        outputText.innerHTML =
+            "Name: " + inputName + ", Length: " + nameLength + " characters";
+    }
+}
+
+function uppercase() {
     let inputName = document.getElementById("nameBox").value;
     let outputText = document.getElementById("nameOutputText");
-    if (inputName == "") {
-        outputText.innerHTML = "Please enter a name!"
+    let nameLength = inputName.length;
 
-    } else {
-        outputText.innerHTML = "Name: " + inputName;
-    }
-};
+    inputName = inputName.trim();
+    inputName = inputName.toUpperCase();
 
-function getAddAnswer(){
-    let firstNumber = Number(document.getElementById("first").value);
-    let secondNumber = Number(document.getElementById("second").value);
+    outputText.innerHTML =
+        "Name: " + inputName + ", Length: " + nameLength + " characters";
+}
+
+function lowercase() {
+    let inputName = document.getElementById("nameBox").value;
+    let outputText = document.getElementById("nameOutputText");
+    let nameLength = inputName.length;
+
+    inputName = inputName.trim();
+    inputName = inputName.toLowerCase();
+
+    outputText.innerHTML =
+        "Name: " + inputName + ", Length: " + nameLength + " characters";
+}
+
+function normal() {
+    let inputName = document.getElementById("nameBox").value;
+    let outputText = document.getElementById("nameOutputText");
+    let nameLength = inputName.length;
+
+    outputText.innerHTML =
+        "Name: " + inputName + ", Length: " + nameLength + " characters";
+}
+
+function getAddAnswer() {
+    let firstNumber = Number(document.getElementById("firstAdd").value);
+    let secondNumber = Number(document.getElementById("secondAdd").value);
     let sumText = document.getElementById("additionSum");
-    if (firstNumber, secondNumber == ""){
+    if ((firstNumber, secondNumber == "")) {
         sumText.innerHTML = "Please enter 2 numbers!";
     } else {
         let sum = firstNumber + secondNumber;
-        sumText.innerHTML = "The answer is: " + sum; 
+        sumText.innerHTML = "The answer is: " + sum;
     }
-};
+}
 
 let count = 0;
-function addCounter(){
+function addCounter() {
     count += 1;
     document.getElementById("countSum").innerHTML = count;
-};
-function resetCounter(){
+}
+function resetCounter() {
     count = 0;
     document.getElementById("countSum").innerHTML = count;
-};
-function subtractCounter(){
+}
+function subtractCounter() {
     count -= 1;
     document.getElementById("countSum").innerHTML = count;
-};
+}
+
+function getRanNum() {
+    let max = document.getElementById("max").value;
+    let answer = document.getElementById("ranAnswer");
+    if (max == "") {
+        answer.innerHTML = "Please enter a max number!";
+    } else {
+        let ranNum = Math.floor(Math.random() * max) + 1;
+        answer.innerHTML = ranNum;
+    }
+}
+
+let toggleCheckBox = document.getElementById("checkBoxToggle")
+let checkText = document.getElementById("checkText")
+
+if (toggleCheckBox.checked == true) {
+    checkText.innerHTML = "On";
+} else {
+    checkText.innerHTML = "Off";
+}
